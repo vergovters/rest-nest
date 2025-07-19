1a1032d8afb280744ecb42b1bb277c804e57163d
vergovters <fialkivskiji@gmail.com>
2025-07-20 00:00:42 +0300

    service and config

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly prisma: PrismaService) {}
-    signin() {
-        // Logic for user signin
-        return { message: 'User signed in successfully' };
+    constructor(private readonly prisma: PrismaService) { }
+    async signin(dto: AuthDto) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email,
+            },
+        });
+        if (!user) {
+            throw new ForbiddenException('Credentials incorrect');
+        }
+        const pwMatches = await argon.verify(user.hash, dto.password);
+        if (!pwMatches) {
+            throw new ForbiddenException('Credentials incorrect');
+        }
+        return user;
     }
 
-    signup() {
-        // Logic for user signup
-        return { message: 'User signed up successfully' };
+    async signup(dto: AuthDto) {
+        const hashedPassword = await argon.hash(dto.password);
+
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash: hashedPassword,
+                }
+            });
+            return user;
+        } catch (error) {
+            if(error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Email already exists');
+                }
+            }
+            throw error;
+        }
     }
 }
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor() {
+    constructor(config: ConfigService) {
         super({
             datasources: {
                 db: {
-                    url: process.env.DATABASE_URL || 'postgresql://devuser:devpass@localhost:5432/rest-nest',
+                    url: config.get('DATABASE_URL'),
                 }
             },
         });
